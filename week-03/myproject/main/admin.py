f19f3091ebb94d4826d30f5854cf378f24eede0e
Eleni Ntzani <ds21116@go.uop.gr>
2025-12-17 16:16:22 +0200

    Delete week-03/myproject/main directory

--- week-03/myproject/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('entry_number', 'title', 'author', 'publish_year', 'isbn')
-    search_fields = ('title', 'author', 'isbn')
